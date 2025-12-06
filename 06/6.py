61727a9d47d530430a37e9a47d134e04486a0230
Giovanni Go <dreamnid@dreamnid.com>
2025-12-05 22:57:36 -0800

    Day 6 Pt 2

--- 06/6.py
+++ 06/6.py
@@ -41,11 +41,48 @@ with PrintTiming('a'):
         match line[-1]:
             case '*':
                 op = operator.mul
+                fill = 1
             case '+':
                 op = operator.add
+                fill = 0
             case _:
                 raise ValueError()
+
+        max_digits = 0
+        for el in line[:-1]:
+            if len(el) > max_digits:
+                max_digits = len(el)
 
         total += reduce(op, list(map(int, line[:-1])))
 
 print('a', total)
+
+input = [line for line in get_file_contents(INPUT_FILE)[0]]
+with PrintTiming('b'):
+    total = 0
+    width = max(len(l) for l in input)
+    height = len(input) - 1
+    buffers = [''] * height
+    for i in range(width):
+        cur_char = input[-1][i] if i < len(input[-1]) else ' '
+        match cur_char:
+            case '*':
+                op = operator.mul
+            case '+':
+                op = operator.add
+            case ' ':
+                pass
+
+        for j in range(height):
+            try:
+                buffers[i % height] += input[j][i]
+            except IndexError:
+                pass
+
+        if i + 1 == width or (i+1 < len(input[-1]) and input[-1][i+1] in ('*', '+')):
+            # process
+            problem_solution = reduce(op, [int(n) for n in buffers if n.strip()])
+            total += problem_solution
+            buffers = [''] * height
+
+print('b', total)
